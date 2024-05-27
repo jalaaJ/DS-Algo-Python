f8bbc7f10039125044634e6c40b692fccbbe2359
jalaaJ <abusalehjalaa1@gmail.com>
2024-05-27 23:05:00 +0300

    Created a method to print the values of the linked list

--- linkedList.py
+++ linkedList.py
@@ -10,4 +10,10 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
         
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
         
