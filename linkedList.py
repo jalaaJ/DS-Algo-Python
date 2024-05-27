114f21e5121e4e2c4a6c4015e01887f480c26902
jalaaJ <abusalehjalaa1@gmail.com>
2024-05-27 23:22:34 +0300

    Created the append method

--- linkedList.py
+++ linkedList.py
@@ -10,6 +10,16 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
         
+    def append(self, value):
+        new_node = Node(value)
+        if self.length >= 1:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+        else:
+            self.head = new_node
+            self.tail = new_node
+                
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -17,3 +27,7 @@ class LinkedList:
             temp = temp.next
         
         
+my_linkedlist = LinkedList(5)
+my_linkedlist.append(4)
+my_linkedlist.append(3)
+my_linkedlist.print_list()
