90ff008b69da21c3522b6783400c2f038425c921
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-03 15:48:41 +0300

    Implemented the append method

--- d_linkedlist.py
+++ d_linkedlist.py
@@ -11,6 +11,21 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
         
+    # We're returning a boolean value because we will use it in another method    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+        
+        
     def print_list(self):
         temp = self.head
         while temp is not None:
