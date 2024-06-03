f6a0df7a14374bdedc85ef0fb6285221b9474ecd
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-03 16:57:58 +0300

    Implemented the pop method

--- d_linkedlist.py
+++ d_linkedlist.py
@@ -23,8 +23,20 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
-        
-        
+    
+    def pop(self):
+        to_delete_node = self.tail
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            to_delete_node.prev = None
+        self.length -= 1
+        return to_delete_node
         
     def print_list(self):
         temp = self.head
@@ -32,7 +44,9 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
         
-        
 my_d_linkedlist = DoublyLinkedList(5)
+my_d_linkedlist.append(1)
+my_d_linkedlist.append(10)
+my_d_linkedlist.pop()
 my_d_linkedlist.print_list()
         
