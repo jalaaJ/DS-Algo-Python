f1fe3fd0c06152447de8fe1b1ab64aaa81ce4003
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-06 19:30:50 +0300

    Implemented the insert and the contains methods

--- bs_tree.py
+++ bs_tree.py
@@ -8,3 +8,45 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
         
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        else:
+            temp = self.root
+            while True:
+                if new_node.value == temp.value:
+                    return False
+                if new_node.value < temp.value:
+                    if temp.left is None:
+                        temp.left = new_node
+                        return True
+                    temp = temp.left
+                else:
+                    if temp.right is None:
+                        temp.right = new_node
+                        return True
+                    temp = temp.right
+    
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if value == temp.value:
+                return True
+            elif value < temp.value:
+                temp = temp.left
+            else:
+                temp = temp.right
+        return False
+            
+my_bst = BinarySearchTree()
+my_bst.insert(5)
+my_bst.insert(10)
+my_bst.insert(2)
+print(my_bst.root.value)
+print(my_bst.root.left.value)
+print(my_bst.root.right.value)
+print(my_bst.contains(10))
