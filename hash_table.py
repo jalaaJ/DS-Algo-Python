fb9bf4bac2c197a5a1018ac23937a71fcc43d3e0
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-07 10:47:03 +0300

    Implemented the hash function

--- hash_table.py
+++ hash_table.py
@@ -2,3 +2,10 @@ class HashTable:
     def __init__(self, size=7):
         self.data_map = [None] * size
         
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(key) * 23) % len(self.data_map)
+        return my_hash
+    
+    
