9ceed0355040c69b7041caa2189715bb5c49c647
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-07 15:33:59 +0300

    Updated the hash function and implemented the get item method

--- hash_table.py
+++ hash_table.py
@@ -5,7 +5,7 @@ class HashTable:
     def __hash(self, key):
         my_hash = 0
         for letter in key:
-            my_hash = (my_hash + ord(key) * 23) % len(self.data_map)
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
     
     def set_item(self, key, value):
@@ -14,8 +14,20 @@ class HashTable:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
         
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    value = self.data_map[index][i][1]
+                    return value
+        return None
+        
     def print_hashtable(self):
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
           
-
+hash_table = HashTable(7)
+hash_table.set_item("Jalaa", 123)
+hash_table.set_item("Jomana", 321)
+print(hash_table.get_item("Jomana"))
