5d710c620b43e4d36ae92551e12276e3a23d3a96
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-12 20:54:56 +0300

    Implemented the add edge method

--- graph.py
+++ graph.py
@@ -8,9 +8,20 @@ class Graph:
             return True
         return False
     
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+        return False
+
     def print_graph(self):
         for vertex in self.adjacency_list:
             print(vertex, ": ", self.adjacency_list[vertex])
             
-
+my_graph = Graph()
+my_graph.add_vertex("JJ")
+my_graph.add_vertex("Jomana")
+my_graph.add_edge("JJ", "Jomana")
+my_graph.print_graph()
     
