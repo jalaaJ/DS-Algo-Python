8709e8b87c61efe189d5cdc99024b025695bc00c
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-12 21:15:03 +0300

    Implemented the remove edge method

--- graph.py
+++ graph.py
@@ -15,7 +15,15 @@ class Graph:
             return True
         return False
     
-    
+    def remove_edge(self, vertex1, vertex2):
+        if self.vertices_exist(vertex1, vertex2):
+            try:
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
     
     def vertices_exist(self, vertex1, vertex2):
         return vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys()
@@ -28,5 +36,6 @@ my_graph = Graph()
 my_graph.add_vertex("JJ")
 my_graph.add_vertex("Jomana")
 my_graph.add_edge("JJ", "Jomana")
+my_graph.remove_edge("JJ", "J")
 my_graph.print_graph()
     
