12344720f995f70d9f1bb5aefb5918556aeef386
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-14 00:42:33 +0300

    Updated the insert method

--- heap.py
+++ heap.py
@@ -19,7 +19,18 @@ class MaxHeap:
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
-        while current > 0 and self.heap[current] > self.heap[self.heap[self._parent_index(current)]]:
+        while current > 0 and self.heap[current] > self.heap[self._parent_index(current)]:
             self._swap_values(current, self._parent_index(current))
             current = self._parent_index(current)
-        
+
+myHeap = MaxHeap()
+myHeap.insert(99)
+myHeap.insert(72)
+myHeap.insert(61)   
+myHeap.insert(58)
+
+print(myHeap.heap)
+
+myHeap.insert(100)
+
+print(myHeap.heap)
